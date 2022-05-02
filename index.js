6849bb6ab57a4535245cfd616c18bee11afbdf07
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-05-02 09:26:43 -0300

    POST Participants

---
+++ index.js
@@ -0,0 +1,31 @@
+import express, { json } from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+
+app.post("/participants", async (req, res) => {
+    const body = req.body;
+    const participantesNovo = {
+        name: body.name,
+        lastStatus: Date.now()
+    }
+    try {
+        await mongoClient.connect();
+        const database = mongoClient.db("projeto");
+        await database.collection("participantes").insertOne(participantesNovo);
+        res.sendStatus(201);
+        mongoClient.close();
+    }
+    catch (e) {
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+});
+
+app.listen(5000);
+
